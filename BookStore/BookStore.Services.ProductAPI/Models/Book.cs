c2cc4e374328041fa9a8cec7e9615bb26f56ede0
kostya byliuk <pozikosuaru2956@gmail.com>
2023-03-18 22:50:55 +0200

    added some Dto classes

--- BookStore/BookStore.Services.ProductAPI/Models/Book.cs
+++ BookStore/BookStore.Services.ProductAPI/Models/Book.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,20 @@ namespace BookStore.Services.ProductAPI.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string ShortDescription { get; set; }
+        public string Description { get; set; }
         public DateTime YearOfPublication { get; set; }
+        [Range(1, 2000)] // grams
         public double Weight { get; set; }
         public bool AreIllustrations { get; set; }
         public string Cover { get; set; }
         public int NumberOfPages { get; set; }
+        [Range(1, 5000)]
         public decimal Price { get; set; }
         public int InStock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
