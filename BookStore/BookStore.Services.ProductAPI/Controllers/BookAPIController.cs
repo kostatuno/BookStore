295a9fb5b5c46e2319cc8f8e8d4eb8a4f35d5235
kostya byliuk <pozikosuaru2956@gmail.com>
2023-03-20 17:58:50 +0200

    add first controllers

---
+++ BookStore/BookStore.Services.ProductAPI/Controllers/BookAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Services.ProductAPI.Controllers
+{
+    public class BookAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
