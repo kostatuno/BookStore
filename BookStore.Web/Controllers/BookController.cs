44286731a986798dcdb56e169e248b03aa00e34d
kostya byliuk <pozikosuaru2956@gmail.com>
2023-03-25 17:31:36 +0200

    Fixed BookController

--- BookStore.Web/Controllers/BookController.cs
+++ BookStore.Web/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using BookStore.Web.Models;
 using BookStore.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace BookStore.Web.Controllers
 {
@@ -20,35 +22,55 @@ namespace BookStore.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            var list = new List<BookDto>();
             var response = await _bookService.GetAllBooksAsync<ResponseDto>();
-            return View(response);
+            if (response is not null && response.IsSuccess == true)
+            {
+                list = JsonConvert.DeserializeObject<List<BookDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetById(int id)
         {
+            var bookDto = new BookDto();
             var response = await _bookService.GetBookByIdAsync<ResponseDto>(id);
+            if (response is not null && response.IsSuccess == true)
+            {
+                bookDto = JsonConvert.DeserializeObject<BookDto>(Convert.ToString(response.Result));
+            }
             return View(response);
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
+            object result = new();
             var response = await _bookService.DeleteBookAsync<ResponseDto>(id);
-            return View(response);
+            if (response is not null && response.IsSuccess == true)
+            {
+                result = JsonConvert.DeserializeObject<BookDto>(Convert.ToString(response.Result));
+            }
+            return View(result);
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(BookDto bookDto)
         {
-            var response = await _bookService.CreateBookAsync<ResponseDto>(bookDto);
+            var bookDtoResult = new BookDto();
+            var response = await _bookService.CreateBookAsync<ResponseDto>(bookDtoResult);
+            if (response is not null && response.IsSuccess == true)
+            {
+                bookDtoResult = JsonConvert.DeserializeObject<BookDto>(Convert.ToString(response.Result));
+            }
             return View(response);
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(int id)
+        public async Task<IActionResult> Update(BookDto bookDto)
         {
-            var response = await _bookService.DeleteBookAsync<ResponseDto>(id);
+            var response = await _bookService.UpdateBookAsync<ResponseDto>(bookDto);
             return View(response);
         }
 
