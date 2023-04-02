38273e2fe1537b30706ff4175c52d2cc04217c5c
kostya byliuk <pozikosuaru2956@gmail.com>
2023-04-02 23:01:33 +0300

    add new tests

---
+++ BookStore.Services.BookAPI/Mapping/MappingConfig.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BookStore.Services.BookAPI.Models;
+using BookStore.Services.BookAPI.Models.Dto;
+
+namespace BookStore.Services.BookAPI.Mapping
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Book, BookDto>()
+                    .ForMember(dst => dst.Genre, opt =>
+                    opt.MapFrom(src => src.Genre.Name));
+                config.CreateMap<BookDto, Book>()
+                    .ForMember(dst => dst.NumberOfPages, opt => opt.Ignore())
+                    .ForMember(dst => dst.YearOfPublication, opt => opt.Ignore())
+                    .ForMember(dst => dst.InStock, opt => opt.Ignore())
+                    .ForMember(dst => dst.AreIllustrations, opt => opt.Ignore())
+                    .ForMember(dst => dst.Cover, opt => opt.Ignore())
+                    .ForMember(dst => dst.Weight, opt => opt.Ignore())
+                    .ForMember(dst => dst.GenreId, opt => opt.Ignore())
+                    .ForMember(dst => dst.Genre, opt => opt.MapFrom(src => new Genre() { Name = src.Genre }));
+
+            });
+
+            return mappingConfig;
+        }
+    }
+}
--- BookStore.Web/Models/BookDto.cs
+++ BookStore.Web/Models/BookDto.cs
@@ -8,5 +8,19 @@ namespace BookStore.Web.Models
         public string Description { get; set; } 
         public string Genre { get; set; }
         public string ImageUrl { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not BookDto obj2 || !obj.GetType().Equals(GetType()))
+            {
+                return false;
+            }
+            return obj2.BookId == BookId &&
+                obj2.Name == Name &&
+                obj2.Price == Price &&
+                obj2.Description == Description &&
+                obj2.Genre == Genre && 
+                obj2.ImageUrl == ImageUrl;
+        }
     }
 }
